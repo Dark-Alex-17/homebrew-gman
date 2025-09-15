535b5941b709fbba5adf6fe928f4de5b9f0b828e
Dark-Alex-17 <alex.j.tusa@gmail.com>
2025-09-15 01:49:42 +0000

    Update formula for G-Man release 0.1.0

--- Formula/gman.rb
+++ Formula/gman.rb
@@ -1,17 +1,17 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-class Gman < Formula
+class GMan < Formula
   desc "Universal command line credential management and injection tool"
   homepage "https://github.com/Dark-Alex-17/gman"
   if OS.mac? and Hardware::CPU.arm?
     url "https://github.com/Dark-Alex-17/gman/releases/download/v0.1.0/gman-aarch64-apple-darwin.tar.gz"
-    sha256 "038ab83638ea5cb0c345745332728561be998af55243d0256931cfaf0a104b95"
+    sha256 "04e5a5349a1577d95a297cd9975ec91423d01d16a68fccddc93371fe7e9ccf6b"
   elsif OS.mac? and Hardware::CPU.intel?
     url "https://github.com/Dark-Alex-17/gman/releases/download/v0.1.0/gman-x86_64-apple-darwin.tar.gz"
-    sha256 "02dffa969c9db815e773d2007c6fd122b7a61aa1e41d2e8ccdf57f93bf9aa0ae"
+    sha256 "f94c2f7c665d2f1098c90535ebb49c574e12e0da5c56c248b9da1e640b857e97"
   else
     url "https://github.com/Dark-Alex-17/gman/releases/download/v0.1.0/gman-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "48fcd51ae899e8c44b74a8a58c9084c97043426ad8d1694d28fb3671308c85f7"
+    sha256 "2e5e9235b9108fc2c5ad8742a7f9c02235efb1b358e3c290d8a8ef708632b5cd"
   end
   version "0.1.0"
   license "MIT"
