c2110f93bc7e0e318c4ba00543b7bddbdb70b93c
Dark-Alex-17 <alex.j.tusa@gmail.com>
2025-09-15 21:25:38 +0000

    Update formula for G-Man release 0.1.0

--- Formula/gman.rb
+++ Formula/gman.rb
@@ -5,13 +5,13 @@ class Gman < Formula
   homepage "https://github.com/Dark-Alex-17/gman"
   if OS.mac? and Hardware::CPU.arm?
     url "https://github.com/Dark-Alex-17/gman/releases/download/v0.1.0/gman-aarch64-apple-darwin.tar.gz"
-    sha256 "95fc67dabd208218d8412b75d1008c59e65d20fc7041f55f77c83fba297512bb"
+    sha256 "63c8b5783bb651f1e2e2431ac208ce8efe59272325ea69ff206b8797a79623e2"
   elsif OS.mac? and Hardware::CPU.intel?
     url "https://github.com/Dark-Alex-17/gman/releases/download/v0.1.0/gman-x86_64-apple-darwin.tar.gz"
-    sha256 "2f7fe7be6a1698186e272795995994d0df2b7f75d50089c7b44eecfeb1162fd5"
+    sha256 "750ec10b050f98505b04b86cbe76ca6fc928d6326d14c0ffa0c3ed11ddb8267a"
   else
     url "https://github.com/Dark-Alex-17/gman/releases/download/v0.1.0/gman-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "78df5a3e547853f00a892656ef801e58d2efe6f45ca2c02956728c22b34c4532"
+    sha256 "274214afb284b9c60e85459321f4c2ee314d1f60241f278305e3b20be9e6f629"
   end
   version "0.1.0"
   license "MIT"
